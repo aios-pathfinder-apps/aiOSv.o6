1fb0fdbdd66a4a663d96c4d92182b2ebc177821f
ShafikAF <ms@afterflea.com>
2023-07-23 13:51:36 +0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,8 +1,8 @@
 module.exports = {
-    CLIENT_ID: '488721198217-f3ora86g414268jsctvt6so1hiug0fre.apps.googleusercontent.com',
+    CLIENT_ID: '487561976896-1pbgbu4b9kjuj4ig9ieap189pievji3c.apps.googleusercontent.com',
     AGENT_SUPERVISOR:"https://agent-supervsior-lvija5gn3q-uc.a.run.app/",
     AGENT_MANAGER: "https://aios-pathfinder-image-lvija5gn3q-uc.a.run.app/",
-    DIGITAL_BRAIN: "https://aios-universal-brain-lvija5gn3q-uc.a.run.app/",
+    DIGITAL_BRAIN: "https://aios-universal-brain-s32ghr3s6q-uc.a.run.app",
     TRANSLATOR: "https://translator-opfcbv5e7q-wl.a.run.app/process_audio/",
     AIOS_AGENT: "https://aios-agent-lvija5gn3q-uc.a.run.app",
     FEEDBACK: "https://docs.google.com/forms/d/e/1FAIpQLScHIKeRwjO24HwNYKMkNu3bBkE2YeuyXI0dhoFY8B9uuiYriQ/viewform"
