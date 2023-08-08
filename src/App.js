0a8352cafa2583f9762c5fc5abc41ea36bfed74a
Noah Grimaldi <noahgrimaldi1@gmail.com>
2023-08-08 12:20:34 -0400

    "Editing particles, fixing errors"

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   const [profile, setProfile] = useState([]);
 
   useEffect(() => {
-    particlesJS.load('particles-js', './particles.json', function() {
+    particlesJS.load('particles-js', 'particles.json', function() {
       console.log('callback - particles.js config loaded');
     });
   }, []);
