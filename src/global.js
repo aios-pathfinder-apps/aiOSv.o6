a20113f35bbae59b68c6bee97300cfc4052e6699
Mohamed Shafik <89096908+MohamedShafik8@users.noreply.github.com>
2023-08-14 23:40:57 +0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,9 +1,9 @@
 module.exports = {
     CLIENT_ID: '356693472207-tjqcuje1bmtcs5jpdk08f6ol7j81p302.apps.googleusercontent.com',
-    AGENT_SUPERVISOR:"https://agent-supervsior-lvija5gn3q-uc.a.run.app/",
+    AGENT_SUPERVISOR:"https://aios-agent-supervisor-2imn7pfmma-uc.a.run.app",
     AGENT_MANAGER: "https://aios-pathfinder-image-lvija5gn3q-uc.a.run.app/",
     DIGITAL_BRAIN: "https://digitalbrain-2imn7pfmma-uc.a.run.app/",
     TRANSLATOR: "https://translator-opfcbv5e7q-wl.a.run.app/process_audio/",
-    AIOS_AGENT: "https://aios-agent-lvija5gn3q-uc.a.run.app",
+    AIOS_AGENT: "https://aios-agent-2imn7pfmma-uc.a.run.app",
     FEEDBACK: "https://docs.google.com/forms/d/e/1FAIpQLScHIKeRwjO24HwNYKMkNu3bBkE2YeuyXI0dhoFY8B9uuiYriQ/viewform"
 }
