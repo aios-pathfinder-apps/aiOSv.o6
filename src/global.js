42b7f8f8a28aa7f4cd6187c1464ef8910cb1fe2e
Mohamed Shafik <89096908+MohamedShafik8@users.noreply.github.com>
2023-10-25 10:51:28 +0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,6 +1,6 @@
 module.exports = {
     CLIENT_ID: '290285315396-1hklne287q0qfm8sjqjcpfdpkutfr26r.apps.googleusercontent.com',
-    Conflict:"https://styled-conf-xhbphobypa-uc.a.run.app/",
+    Conflict:"https://test1-xhbphobypa-uc.a.run.app/",
     Psychaid: "https://psychaid-new-xhbphobypa-uc.a.run.app/",
     ChildCare: "https://childsage-new-xhbphobypa-uc.a.run.app/",
     ElderCare: "https://eldercare-xhbphobypa-uc.a.run.app/",
