f0e8399661b832722c8730892ea0485999ed33db
Mohamed Shafik <89096908+MohamedShafik8@users.noreply.github.com>
2023-10-31 16:55:17 +0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,9 +1,9 @@
 module.exports = {
     CLIENT_ID: '290285315396-1hklne287q0qfm8sjqjcpfdpkutfr26r.apps.googleusercontent.com',
-    Conflict:"https://test1-xhbphobypa-uc.a.run.app/",
+    Conflict:"https://unityseeker1-xhbphobypa-uc.a.run.app",
     Psychaid: "https://psychaid-new-xhbphobypa-uc.a.run.app/",
-    ChildCare: "https://childsage-new-xhbphobypa-uc.a.run.app/",
-    ElderCare: "https://eldercare-xhbphobypa-uc.a.run.app/",
+    ChildCare: "https://child-stream-xhbphobypa-uc.a.run.app",
+    ElderCare: "https://elder-stream-xhbphobypa-uc.a.run.app/",
     AIOS_AGENT: "https://aios-agent-2imn7pfmma-uc.a.run.app",
     FEEDBACK: "https://docs.google.com/forms/d/e/1FAIpQLScHIKeRwjO24HwNYKMkNu3bBkE2YeuyXI0dhoFY8B9uuiYriQ/viewform"
 }
